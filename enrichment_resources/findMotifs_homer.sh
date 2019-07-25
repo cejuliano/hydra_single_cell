1be36813bf2f5c97e7f151d99b2c493c78907d66
StefanS <stef.siebert@gmail.com>
2019-07-25 10:46:49 -0700

    update enrichment_resources

--- enrichment_resources/findMotifs_homer.sh
+++ enrichment_resources/findMotifs_homer.sh
@@ -2,8 +2,9 @@
 for arg in "$@"
 do
 	findMotifsGenome.pl "$arg" \
-	../resources/Hm105_Dovetail_Assembly_1.0.fasta \
+	../enrichment_resources/Hm105_Dovetail_Assembly_1.0.fasta \
 	"${arg/.bed/}" \
-	-size given -p 6 -mknown ../resources/Hydra_PFMs/Hydra.motifs \
-	-mcheck ../resources/Hydra_PFMs/Hydra.motifs
+	-size given -p 6 -mknown ../enrichment_resources/Hydra_PFMs/Hydra.motifs \
+	-nomotif \
+	-mcheck ../enrichment_resources/Hydra_PFMs/Hydra.motifs
 done
